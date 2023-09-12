e635af5449944968671119a026fea70f7be7ded9
Daniel Ma <dma@planningcenter.com>
2023-09-11 22:13:38 -0700

    Initial Commit

---
+++ Idyll/IdyllApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdyllApp.swift
+//  Idyll
+//
+//  Created by Daniel Ma on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IdyllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
