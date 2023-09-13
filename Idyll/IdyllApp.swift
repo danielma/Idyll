1be59d7f974f72a828468c0a9c2c6f3119ba9b0e
Daniel Ma <dma@planningcenter.com>
2023-09-12 22:47:06 -0700

    persist

--- Idyll/IdyllApp.swift
+++ Idyll/IdyllApp.swift
@@ -9,28 +9,70 @@ import SwiftUI
 
 let initialAmounts = idyllResources.map { _ in Double(0) }
 
+struct SavedGameState: Codable {
+    var purchasedAmounts = initialAmounts
+    var resourceAmounts = initialAmounts
+    var savedAt = Date()
+    var totalAmount: Double = 10
+}
+
+class GameStore {
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("gameState.data")
+    }
+
+
+    func load() async throws -> SavedGameState {
+        let task = Task<SavedGameState, Error> {
+            let fileURL = try Self.fileURL()
+            guard let data = try? Data(contentsOf: fileURL) else {
+                return SavedGameState()
+            }
+            let dailyScrums = try JSONDecoder().decode(SavedGameState.self, from: data)
+            return dailyScrums
+        }
+        let scrums = try await task.value
+       return scrums
+    }
+
+    func save(state: GameState) async throws {
+            let task = Task {
+                let saveState = SavedGameState(purchasedAmounts: state.purchasedAmounts, resourceAmounts: state.resourceAmounts, savedAt: Date(), totalAmount: state.totalAmount)
+                let data = try JSONEncoder().encode(saveState)
+                let outfile = try Self.fileURL()
+                try data.write(to: outfile)
+            }
+            _ = try await task.value
+        }
+}
+
 class GameState: ObservableObject {
     @Published var purchasedAmounts = initialAmounts
     @Published var resourceAmounts = initialAmounts
+    @Published var totalAmount: Double = 10
     
     var resources = idyllResources
 }
 
 @main
 struct IdyllApp: App {
+    private var store = GameStore()
     @StateObject var model = GameState()
-    @State var totalAmount: Double = 10
     
     private func setTotalAmount(_ value: Double) {
-        totalAmount = value
+        model.totalAmount = value
         lastSetTotalAmount = Date()
-        lastTotalAmount = value
     }
     
     let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
-    @State var lastTotalAmount: Double = 0
     @State var lastSetTotalAmount = Date()
     
+    @Environment(\.scenePhase) private var scenePhase
+
     private func perSecond() -> Double {
         let resource = model.resources[0]
         let amount = model.resourceAmounts[0]
@@ -44,36 +86,58 @@ struct IdyllApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView(
-                totalAmt: totalAmount,
-                resources: model.resources,
+                totalAmt: model.totalAmount,
+                resources: idyllResources,
                 amounts: model.resourceAmounts,
                 purchasedAmounts: model.purchasedAmounts,
                 perSecond: perSecond(),
                 buyResource: { idx in
-                let purchasedAmt = model.purchasedAmounts[idx]
-                let cost = model.resources[idx].currentCost(purchasedAmt)
-                
-                if (totalAmount >= cost) {
-                    model.resourceAmounts[idx] += 1
-                    model.purchasedAmounts[idx] += 1
-                    setTotalAmount(totalAmount - cost)
+                    let purchasedAmt = model.purchasedAmounts[idx]
+                    let cost = idyllResources[idx].currentCost(purchasedAmt)
+
+                    if (model.totalAmount >= cost) {
+                        model.resourceAmounts[idx] += 1
+                        model.purchasedAmounts[idx] += 1
+                        setTotalAmount(model.totalAmount - cost)
+                    }
+                }
+            )
+            .task {
+                do {
+                    let state = try await store.load()
+
+                    model.purchasedAmounts = state.purchasedAmounts
+                    model.resourceAmounts = state.resourceAmounts
+                    lastSetTotalAmount = state.savedAt
+                } catch {
+                    fatalError(error.localizedDescription)
                 }
             }
-            ).onReceive(timer) { _ in
+            .onReceive(timer) { _ in
                 let now = Date()
                 let secondsBeenRunning = lastSetTotalAmount.distance(to: now)
                 
                 for (index) in model.resources.indices {
-                    let resource = model.resources[index]
+                    let resource = idyllResources[index]
                     let amount = model.resourceAmounts[index]
                     let purchasedAmount = model.purchasedAmounts[index]
                     let stepMultiplier = resource.stepMultiplier(purchasedAmount)
                     let countToAdd = secondsBeenRunning * amount * stepMultiplier
                     
                     if (index == 0 ) {
-                        setTotalAmount(totalAmount + countToAdd)
+                        setTotalAmount(model.totalAmount + countToAdd)
                     } else {
                         model.resourceAmounts[index - 1] += countToAdd
+                    }
+                }
+            }
+        }.onChange(of: scenePhase) { phase in
+            if (phase != .active) {
+                Task {
+                    do {
+                        try await store.save(state: model)
+                    } catch {
+                        fatalError(error.localizedDescription)
                     }
                 }
             }
---
+++ NOTES.md
@@ -0,0 +1,7 @@
+# TODO
+
+* [x] Persist
+* [ ] Cap idle earnings
+* [ ] Figure out when next is available (maybe just as soon as you own any)
+* [ ] Buy max / 10
+* [ ] Half-filled button to reflect available money
